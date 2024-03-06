0f77dc57c2cf83a8c7fc62d2689b58dffb1e500f
Harlen Kinsey <harlenkinsey60@gmail.com>
2024-03-06 14:11:53 -0600

    updated credits

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,15 @@
 import React, {Fragment} from 'react'
 
 export const About = () => {
+    
+    const today = new Date();
+
     return (
         <Fragment>
             <h1>About This App</h1>
             <p>App to search Github users</p>
             <p>Version: 1.0.0</p>
+            <p>Developer: Harlen Kinsey {today.getFullYear()}</p>
         </Fragment>
     )
 }
